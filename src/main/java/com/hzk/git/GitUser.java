489a1cd16263bbd527dfbfbb766a9eafa9f8029e
huangsir <1059308740@qq.com>
2021-09-12 15:56:23 +0800

    hot-fix commit3

--- src/main/java/com/hzk/git/GitUser.java
+++ src/main/java/com/hzk/git/GitUser.java
@@ -11,5 +11,6 @@ public class GitUser {
         System.out.println("hello git3");
         System.out.println("hello git4");
         System.out.println("hot-fix test");
+        System.out.println("hot-fix test2");
     }
 }

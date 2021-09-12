dbd51783c9aef014b49932537ee554b51f7d38a1
huangsir <1059308740@qq.com>
2021-09-12 15:29:52 +0800

    first commit

---
+++ src/main/java/com/hzk/git/GitUser.java
@@ -0,0 +1,11 @@
+package com.hzk.git;
+
+/**
+ * @author huangSir
+ * @version 1.0
+ */
+public class GitUser {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
